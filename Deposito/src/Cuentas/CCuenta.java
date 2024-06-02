692d4f8b10fff217df3ce49d91e5a81df28f20b3
Dayson <Dayson@LAPTOP-OUI6KUJ9>
2024-06-02 16:18:35 +0200

    Segundo commit para subir el javadoc con toda la información completa.

--- Deposito/src/Cuentas/CCuenta.java
+++ Deposito/src/Cuentas/CCuenta.java
@@ -1,11 +1,18 @@
 package Cuentas;
+/**
+ * @author Dayson
+ * Entornos de Desarrollo Tarea 4 
+ * Esto es una clase que representa una cuenta bancaria
+ */
 public class CCuenta {
-
+/**
+ * Cuenta bancaria muy simple compuesta por pocos atributos nombre,cuenta,saldo y tipo de interés
+ */
 
     private String nombre;
     private String cuenta;
     private double saldo;
-    private double tipoInterés;
+    private double tipoInteres;
 
     public CCuenta()
     {
@@ -48,21 +55,29 @@ public class CCuenta {
 		this.saldo = saldo;
 	}
 
-	public double getTipoInterés() {
-		return tipoInterés;
+	public double getTipoInteres() {
+		return tipoInteres;
 	}
 
-	public void setTipoInterés(double tipoInterés) {
-		this.tipoInterés = tipoInterés;
+	public void setTipoInteres(double tipoInteres) {
+		this.tipoInteres = tipoInteres;
 	}
-
+/**
+ * Metodo para ingresar una cantidad que nue puede ser negativa
+ * @param cantidad
+ * @throws Exception
+ */
 	public void ingresar(double cantidad) throws Exception
     {
         if (cantidad<0)
             throw new Exception("No se puede ingresar una cantidad negativa");
         saldo = saldo + cantidad;
     }
-
+/**
+ * Metodo para retirar una cantidad, no puede ser negativa ni mayor al saldo existente
+ * @param cantidad
+ * @throws Exception
+ */
     public void retirar(double cantidad) throws Exception
     {
         if (cantidad <= 0)
